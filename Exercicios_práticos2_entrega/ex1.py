fe8ff5c2e3ba59f7db3d2b6f67049d7ae1c059b1
José Rodrigues <joserodriguesrs18@gmail.com>
2018-03-21 21:54:46 -0700

    Pronto if [0-20] notas

--- Exercicios_práticos2_entrega/ex1.py
+++ Exercicios_práticos2_entrega/ex1.py
@@ -14,10 +14,10 @@ elif  nota1 < MIN or nota2 < MIN or nota3 < MIN:
         print("atuamae2")  
 elif nota1 >= MIN and nota2 >= MIN and nota3 >= MIN:
     if nota1 <= MAX and nota2 <= MAX and nota3 <= MAX:
-        nota1_1 = nota1 * 25
-        nota2_2 = nota2 * 35
-        nota3_3 = nota3 * 40 
-        notafinal = (nota1_1 + nota2_2 + nota3_3) / 100 
+        nota1 *= 25
+        nota2 *= 35
+        nota3 *= 40 
+        notafinal = (nota1 + nota2 + nota3) / 100 
         print("A tua média é igual a: ",notafinal)    
         if notafinal >= 9.5:
             print("Por isso estás Aprovado")
