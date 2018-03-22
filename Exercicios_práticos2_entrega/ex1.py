5f191693a08e7b48d18d0317d6a95ff5e2bc11ce
José Rodrigues <joserodriguesrs18@gmail.com>
2018-03-21 21:52:39 -0700

    Atualizado e pronto

--- Exercicios_práticos2_entrega/ex1.py
+++ Exercicios_práticos2_entrega/ex1.py
@@ -5,21 +5,21 @@ nota1 = float(input("\nTeste 1 \"25%\": "))
 nota2 = float(input("Teste 1 \"35%\": "))
 nota3 = float(input("Teste 1 \"40%\": "))
 
-nota1 *= 25
-nota2 *= 35
-nota3 *= 40 
+MAX = 20
+MIN = 0
 
-notas = [nota1, nota2, nota3]
-
-notafinal = (nota1 + nota2 + nota3) / 100
-
-if nota1 > 20 or nota2 > 20 or nota3 > 20:
-    print("Colocou algo de errado nas suas notas!")
-elif nota1 < 0 or nota2 < 0 or nota3 < 0:
-    print("Colocou algo de errado nas suas notas!")
-elif notas <= 20 and notas >= 0 and notafinal >= 9.5:
-    print("Aprovado")
-else:
-    print("Reprovado")
-    
-    
+if nota1 > MAX or nota2 > MAX or nota3 > MAX:
+    print("atuamae")
+elif  nota1 < MIN or nota2 < MIN or nota3 < MIN:
+        print("atuamae2")  
+elif nota1 >= MIN and nota2 >= MIN and nota3 >= MIN:
+    if nota1 <= MAX and nota2 <= MAX and nota3 <= MAX:
+        nota1_1 = nota1 * 25
+        nota2_2 = nota2 * 35
+        nota3_3 = nota3 * 40 
+        notafinal = (nota1_1 + nota2_2 + nota3_3) / 100 
+        print("A tua média é igual a: ",notafinal)    
+        if notafinal >= 9.5:
+            print("Por isso estás Aprovado")
+        else:
+            print("Por isso estás Reprovado")
