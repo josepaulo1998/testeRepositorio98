6693bd10826c50ccbb118958b006c231c5a7e8f0
José Rodrigues <joserodriguesrs18@gmail.com>
2018-03-27 17:11:36 -0700

    os nnúmeros antes, não estão a dar

---
+++ Exercicios_práticos3_entrega/ex1.py
@@ -0,0 +1,15 @@
+numero = int(input("Numero: "))
+
+while True:
+    for var in range(5):
+        numero += 1
+        print(numero)
+    break
+
+print()
+
+while True:
+    for var in range(5):
+        numero -= 1
+        print(numero)
+    break
